aa7de2c6ba3126ba990335bd4312a4b9bb586337
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-06-27 23:37:03 +1000

    Modified takeEveryNth to use drop instead of a convoluted tupling solution. Also added an alternative version.

--- Golf.hs
+++ Golf.hs
@@ -1,12 +1,26 @@
 module Golf where
 
+import Control.Category
+
+takeEveryNth :: Int -> [a] -> [a]
+-- Why does left-to-right composition have a verbose operator? Left here as a
+-- note of how to do it.
+takeEveryNth n = drop (n - 1) >>> takeEveryNth' where
+  takeEveryNth' :: [a] -> [a]
+  takeEveryNth' [] = []
+  takeEveryNth' (x:ys) = x : takeEveryNth' (drop (n - 1) ys)
 
 skips :: [a] -> [[a]]
-skips xs = skips' 1 xs
-  where skips' :: Int -> [a] -> [[a]]
-        skips' m ys
-          | m == length xs = [ys]
-          | otherwise = takeEveryNth m ys : skips' (m + 1) ys
-          where takeEveryNth :: Int -> [a] -> [a]
-                takeEveryNth n zs = map snd
-                                    (filter (\(x,_) -> x `mod` n == 0) (zip [1..] zs))
+skips xs = skips' 1 xs where
+  skips' :: Int -> [a] -> [[a]]
+  skips' m ys
+    | m == length xs = [ys] -- Is length xs optimised out?
+    | otherwise = takeEveryNth m ys : skips' (m + 1) ys
+
+-- Alternative version.
+skips'' :: [a] -> [[a]]
+skips'' l = map (f l) [1 .. length l] where
+  f :: [a] -> Int -> [a]
+  f l' n = case drop (n - 1) l' of
+    (x:xs) -> x : f xs n
+    [] -> []
