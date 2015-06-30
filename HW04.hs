4145bb59a62d541cc31c72f0ed1aa395934d05df
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-06-30 22:45:48 +1000

    Playing around with ghc-mod and hlint

--- HW04.hs
+++ HW04.hs
@@ -12,7 +12,7 @@ fun1' (x:xs)
   | otherwise = fun1' xs
 
 fun2 :: Integer -> Integer
-fun2 = foldr (+) 0 . takeWhile (/= 1) . drop 1 . iterate (\n ->
+fun2 = sum . takeWhile (/= 1) . drop 1 . iterate (\n ->
              if even n then n `div` 2 else 3 * n + 1)
 
 fun2' :: Integer -> Integer
@@ -27,23 +27,33 @@ data Tree a = Leaf
 
 insert :: a -> Tree a -> Tree a
 insert x Leaf = Node 0 Leaf x Leaf
-insert x (Node h Leaf y Leaf) = Node 1 (insert x Leaf) y Leaf
-insert x (Node h l y Leaf) = Node 1 l y (insert x Leaf)
+insert x (Node _ Leaf y Leaf) = Node 1 (insert x Leaf) y Leaf
+insert x (Node _ l y Leaf) = Node 1 l y (insert x Leaf)
+insert x (Node _ Leaf y r) = Node 1 (insert x Leaf) y r
 insert x (Node h l@(Node hl _ _ _) y r@(Node hr _ _ _))
   -- We can insert into the left without changing the height of the tree.
   | hl < hr = Node (hr+1) (insert x l) y r
   -- Try and insert into the right. If it's height is the same as the root
   -- update the root's height appropriately.
-  | otherwise = case insert x r of
-                  r'@(Node hr' _ _ _)
-                    | h == hr' -> (Node (h+1) l y r')
-                    | otherwise -> (Node h l y r')
+  | otherwise =
+      case insert x r of
+        r'@(Node hr' _ _ _)
+          | h == hr' -> Node (h+1) l y r'
+          | otherwise -> Node h l y r'
+        _ -> Node h l y r
 
 foldTree :: [a] -> Tree a
 foldTree = foldr insert Leaf
 
+fromBool :: Bool -> Integer
+fromBool False = 0
+fromBool True = 1
+
 xor :: [Bool] -> Bool
-xor = odd . foldr (\x -> (+) (if x then 1 else 0)) 0
+xor = odd . sum . map fromBool
+
+xor' :: [Bool] -> Bool
+xor' = foldr (/=) False
 
 map' :: (a -> b) -> [a] -> [b]
 map' f = foldr ((:) . f) []
@@ -52,4 +62,4 @@ exclude :: Integer -> [Integer]
 exclude n = [k | i <- [1..n], j <- [1..n], i <= j, let k = i+j+2*i*j, k <= n]
 
 sieveSundaram :: Integer -> [Integer]
-sieveSundaram n = map (\x -> 2*x + 1) $ [1..n] \\ exclude
+sieveSundaram n = map (\x -> 2*x + 1) $ [1..n] \\ exclude n
