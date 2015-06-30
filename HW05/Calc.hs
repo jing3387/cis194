8f7929021025fb8f658ef4f910588e7afb06873c
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-06-30 23:44:17 +1000

    Homework 5 Exercises 3-5

--- HW05/Calc.hs
+++ HW05/Calc.hs
@@ -1,7 +1,12 @@
-module HW05 where
+{-# LANGUAGE FlexibleInstances,
+             TypeSynonymInstances,
+             GeneralizedNewtypeDeriving #-}
+
+module Calc where
 
 import ExprT
 import Parser
+import qualified StackVM as S
 
 eval :: ExprT -> Integer
 eval (Lit x) = x
@@ -12,3 +17,61 @@ evalStr :: String -> Maybe Integer
 evalStr s = case parseExp Lit Add Mul s :: Maybe ExprT of
               Just x -> Just $ eval x
               Nothing -> Nothing
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Ord, Eq, Show)
+instance Expr MinMax where
+  lit = MinMax
+  add = max
+  mul = min
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+instance Expr Mod7 where
+  lit = Mod7 . mod 7
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger :: Maybe Integer
+testInteger = testExp
+
+testBool :: Maybe Bool
+testBool = testExp
+
+testMM :: Maybe MinMax
+testMM = testExp
+
+testSat :: Maybe Mod7
+testSat = testExp
+
+instance Expr S.Program where
+  lit x = [S.PushI x]
+  add x y = x ++ y ++ [S.Add]
+  mul x y = x ++ y ++ [S.Mul]
+
+compile :: String -> Maybe S.Program
+compile s = parseExp lit add mul s
